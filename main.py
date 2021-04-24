5a595feac2eb260297bf6e455fb8f160ab80094a
Mnedo <75080258+Mnedo@users.noreply.github.com>
2021-04-24 08:45:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import discord
+from requests import get
+
+TOKEN = "ODMxMTY" + "1MzE4Nzg0NjE0NDIw.YHRRBg.VI70SODj-91ZTPJe" + "GxLFXeiuEzQ"
+
+
+class YLBotClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+        for guild in self.guilds:
+            print(
+                f'{self.user} подключились к чату:\n'
+                f'{guild.name}(id: {guild.id})')
+
+    async def on_member_join(self, member):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f'Привет, {member.name}!'
+        )
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        else:
+            if "stop" in message.content.lower():
+                await 'logout'
+            else:
+                file = get('https://api.thecatapi.com/v1/images/search').json()[0]['url']
+                await message.channel.send(file)
+
+
+client = YLBotClient()
+client.run(TOKEN)
+
